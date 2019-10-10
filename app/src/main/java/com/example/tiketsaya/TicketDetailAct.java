e81df8d7226746fb1c7b6062a4f4804ffdd726f4
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-10 13:43:28 +0700

    make function back button

--- app/src/main/java/com/example/tiketsaya/TicketDetailAct.java
+++ app/src/main/java/com/example/tiketsaya/TicketDetailAct.java
@@ -2,13 +2,28 @@ package com.example.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class TicketDetailAct extends AppCompatActivity {
+
+    LinearLayout btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_detail);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backtohome = new Intent(TicketDetailAct.this, HomeAct.class);
+                startActivity(backtohome);
+            }
+        });
     }
 }
