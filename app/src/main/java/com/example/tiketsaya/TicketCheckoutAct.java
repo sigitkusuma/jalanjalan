e307ad34271b660b165928789951dbbcf9f938c5
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-21 23:51:52 +0700

    ticket detail adding done

--- app/src/main/java/com/example/tiketsaya/TicketCheckoutAct.java
+++ app/src/main/java/com/example/tiketsaya/TicketCheckoutAct.java
@@ -2,13 +2,38 @@ package com.example.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class TicketCheckoutAct extends AppCompatActivity {
+
+    LinearLayout btn_back; Button btn_pay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_checkout);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_pay = findViewById(R.id.btn_pay);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backtodetail = new Intent(TicketCheckoutAct.this, TicketDetailAct.class);
+                startActivity(backtodetail);
+            }
+        });
+
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosuccess = new Intent(TicketCheckoutAct.this, SuccessBuyTicketAct.class);
+                startActivity(gotosuccess);
+            }
+        });
     }
 }
