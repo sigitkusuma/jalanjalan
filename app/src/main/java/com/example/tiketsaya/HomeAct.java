628cde3eaef6d1713801b7dfb80f4851f1009cb3
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-26 01:10:53 +0700

    add myprofile add my ticket fix circle view

--- app/src/main/java/com/example/tiketsaya/HomeAct.java
+++ app/src/main/java/com/example/tiketsaya/HomeAct.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.github.florent37.shapeofview.shapes.CircleView;
+
 public class HomeAct extends AppCompatActivity {
 
     LinearLayout btn_ticket_pisa;
+    CircleView btn_to_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,21 @@ public class HomeAct extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         btn_ticket_pisa = findViewById(R.id.btn_ticket_pisa);
+        btn_to_profile = findViewById(R.id.btn_to_profile);
+
         btn_ticket_pisa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent gotopisaticket= new Intent(HomeAct.this, TicketDetailAct.class);
                 startActivity(gotopisaticket);
+            }
+        });
+
+        btn_to_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoprofile = new Intent(HomeAct.this, MyProfileAct.class);
+                startActivity(gotoprofile);
             }
         });
     }
--- app/src/main/java/com/example/tiketsaya/MyProfileAct.java
+++ app/src/main/java/com/example/tiketsaya/MyProfileAct.java
@@ -2,13 +2,27 @@ package com.example.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MyProfileAct extends AppCompatActivity {
 
+    LinearLayout item_my_ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        item_my_ticket = findViewById(R.id.item_my_ticket);
+
+        item_my_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotomytickets = new Intent(MyProfileAct.this, MyTicketAct.class);
+                startActivity(gotomytickets);
+            }
+        });
     }
 }
