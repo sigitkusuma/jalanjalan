b4ba5295285880591a7206894739bdf827f2c7f6
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-27 01:16:15 +0700

    add edit profil

--- app/src/main/java/com/example/tiketsaya/MyProfileAct.java
+++ app/src/main/java/com/example/tiketsaya/MyProfileAct.java
@@ -5,23 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MyProfileAct extends AppCompatActivity {
 
-    LinearLayout item_my_ticket;
+    LinearLayout item_my_ticket; Button btn_edit_profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
 
         item_my_ticket = findViewById(R.id.item_my_ticket);
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
 
         item_my_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent gotomytickets = new Intent(MyProfileAct.this, MyTicketAct.class);
                 startActivity(gotomytickets);
+            }
+        });
+
+        btn_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoeditprofile = new Intent(MyProfileAct.this, EditProfileAct.class);
+                startActivity(gotoeditprofile);
             }
         });
     }
