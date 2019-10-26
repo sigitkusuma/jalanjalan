ac5f4c184178db60c1336997a21bbfb8aa46a085
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-27 01:35:45 +0700

    fix default activity (Splash ACT)

--- app/src/main/java/com/example/tiketsaya/SplashAct.java
+++ app/src/main/java/com/example/tiketsaya/SplashAct.java
@@ -5,13 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashAct extends AppCompatActivity {
+
+    Animation addsplash;
+    ImageView applogo;
+    TextView apptitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+        //load animation
+        addsplash = AnimationUtils.loadAnimation(this, R.anim.addsplash);
+
+        //load element
+        applogo = findViewById(R.id.applogo);
+        apptitle = findViewById(R.id.apptitle);
 
         //membuat timer
         Handler handler = new Handler();
@@ -23,7 +39,6 @@ public class SplashAct extends AppCompatActivity {
                 startActivity(abc);
                 finish();
             }
-        }, 1000);
-
+        }, 2000); //2000 ms = 2 second
     }
 }
