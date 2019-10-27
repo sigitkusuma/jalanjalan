fe3b7ccd12217deed81c574919508c3e9f5c965b
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-27 21:00:04 +0700

    add koneksi database firebase add register to database

--- app/src/main/java/com/example/tiketsaya/RegisterOneAct.java
+++ app/src/main/java/com/example/tiketsaya/RegisterOneAct.java
@@ -1,16 +1,31 @@
 package com.example.tiketsaya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneAct extends AppCompatActivity {
 
     LinearLayout btn_back; Button btn_continue;
+    EditText username, password, email_address;
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "username_key";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +34,51 @@ public class RegisterOneAct extends AppCompatActivity {
 
         btn_back = findViewById(R.id.btn_back);
         btn_continue = findViewById(R.id.btn_continue);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        email_address = findViewById(R.id.email_address);
+
+
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //menyimpan data secara local
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(username_key, username.getText().toString());
+                editor.apply();
+
+                //simpan ke firebase
+                reference = FirebaseDatabase.getInstance().getReference()
+                        .child("Users").child(username.getText().toString());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().child("username").setValue(username.getText().toString());
+                        dataSnapshot.getRef().child("password").setValue(password.getText().toString());
+                        dataSnapshot.getRef().child("email_address").setValue(email_address.getText().toString());
+                        dataSnapshot.getRef().child("user_balance").setValue(800);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+                Intent gotogegistertwo = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
+                startActivity(gotogegistertwo);
+            }
+        });
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
-            }
-        });
-
-        btn_continue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent gotogegistertwo = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
-                startActivity(gotogegistertwo);
             }
         });
     }
