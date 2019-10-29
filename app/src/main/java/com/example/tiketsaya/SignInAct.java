184029b5d997c7032c2a2852f73f0f75f8d5488e
sigitkusuma <54241510+sigitkusuma@users.noreply.github.com>
2019-10-29 17:34:09 +0700

    user register 2 user login

--- app/src/main/java/com/example/tiketsaya/SignInAct.java
+++ app/src/main/java/com/example/tiketsaya/SignInAct.java
@@ -1,16 +1,32 @@
 package com.example.tiketsaya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignInAct extends AppCompatActivity {
 
     TextView btn_new_account; Button btn_sign_in;
+    EditText xusername, xpassword;
+
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "username_key";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,8 @@ public class SignInAct extends AppCompatActivity {
 
         btn_new_account = findViewById(R.id.btn_new_account);
         btn_sign_in = findViewById(R.id.btn_sign_in);
+        xusername = findViewById(R.id.xusername);
+        xpassword = findViewById(R.id.xpassword);
 
         btn_new_account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,8 +49,50 @@ public class SignInAct extends AppCompatActivity {
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gotohome = new Intent(SignInAct.this, HomeAct.class);
-                startActivity(gotohome);
+
+                String username = xusername.getText().toString();
+                final String password = xpassword.getText().toString();
+                reference = FirebaseDatabase.getInstance().getReference()
+                        .child("Users").child(username);
+
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()){
+
+                            //ambil password dari firebase
+                            String passwordFromFirebase = dataSnapshot.child("password").getValue().toString();
+
+                            //Validasi Password
+                            if (password.equals(passwordFromFirebase)) {
+
+                                //menyimpan data secara local
+                                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString(username_key, xusername.getText().toString());
+                                editor.apply();
+
+                                //berpindah aktivity
+                                Intent gotohome = new Intent(SignInAct.this, HomeAct.class);
+                                startActivity(gotohome);
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Username Tidak Tersedia", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(getApplicationContext(),"Database Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
             }
         });
     }
